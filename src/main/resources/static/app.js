d38766e85f9f1633faad6c08c2c9b993fe52a0da
Maikha Abarca <maikha.abarca@gmail.com>
2017-11-15 10:32:41 +0100

    After Pairing, we moved angular into the SBoot project and the whole stuff.
    Todo:add images and parse all the data from the backend

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,28 @@
+(function() {
+    var app = angular.module('beerStore', []);
+
+    app.controller('StoreController', function($scope, $http) {
+        var url = "/other";
+        $http.get( url
+        ).success(function(data){
+            $scope.store.products = data;
+        });
+    });
+
+    app.controller("GalleryController", function(){
+        this.current = 0;
+    });
+
+    app.controller("TabController", function(){
+        this.current = 1;
+        this.setTab = function(number){
+            this.current = number;
+        }
+        this.isSet = function(number) {
+            return number === this.current;
+        }
+
+    });
+
+})();
+
